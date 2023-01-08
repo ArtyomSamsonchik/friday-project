080322c9f7f2720ff8f66a806e96aedb240191be
ArtyomSamsonchik <colpachek@gmail.com>
2023-01-09 01:47:48 +0300

    feat(profile): add profile slice template

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,12 +2,14 @@ import { applyMiddleware, combineReducers, legacy_createStore as createStore } f
 import thunkMiddleware from 'redux-thunk'
 
 import { authSlice } from '../features/auth/auth-slice'
+import { profileSlice } from '../features/profile/profile-slice'
 
 import { appSlice } from './app-slice'
 
 const rootReducer = combineReducers({
   app: appSlice,
   auth: authSlice,
+  profile: profileSlice,
 })
 
 export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
---
+++ src/features/profile/profile-slice.ts
@@ -0,0 +1,8 @@
+const initState = {}
+
+export const profileSlice = (state = initState, action: any): typeof initState => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
