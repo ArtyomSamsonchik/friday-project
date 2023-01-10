4fb4805728d76a421af95e17ea38de477b68e584
ArtyomSamsonchik <colpachek@gmail.com>
2023-01-10 08:31:55 +0300

    feat(auth): add fetchProfile thunk and login AC in authSlice

--- src/features/auth/auth-slice.ts
+++ src/features/auth/auth-slice.ts
@@ -1,8 +1,32 @@
-const initState = {}
+import { AppThunk } from '../../app/store'
+import { setProfile } from '../profile/profile-slice'
 
-export const authSlice = (state = initState, action: any): typeof initState => {
+import { authAPI } from './auth-api'
+
+const initState = {
+  isLoggedIn: false,
+}
+
+export const authSlice = (state = initState, action: ActionsType): typeof initState => {
   switch (action.type) {
+    case 'auth/login':
+      return { ...state, isLoggedIn: true }
     default:
       return state
   }
 }
+
+export const login = () => ({ type: 'auth/login' } as const)
+
+export const fetchProfile =
+  (email: string, password: string): AppThunk =>
+  async dispatch => {
+    const { data } = await authAPI.login(email, password)
+
+    dispatch(setProfile(data))
+  }
+
+type LoginAT = ReturnType<typeof login>
+type ActionsType = LoginAT
+
+//TODO: add app status processing via dispatching in every thunk
