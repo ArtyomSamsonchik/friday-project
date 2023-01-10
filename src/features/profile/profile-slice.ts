4b21f431684ecbfcdd5ce85ad9322c6e2c4386d1
ArtyomSamsonchik <colpachek@gmail.com>
2023-01-10 08:41:23 +0300

    feat(profile): add logic to setting profile data in store

--- src/features/profile/profile-slice.ts
+++ src/features/profile/profile-slice.ts
@@ -1,8 +1,28 @@
-const initState = {}
+import { RootStateType } from '../../app/store'
+import { User } from '../auth/auth-api'
 
-export const profileSlice = (state = initState, action: any): typeof initState => {
+const initState = {
+  userData: null as User | null,
+}
+
+export const profileSlice = (state = initState, action: ActionsType): typeof initState => {
   switch (action.type) {
+    case 'profile/loaded':
+      return { ...state, userData: action.payload }
     default:
       return state
   }
 }
+
+export const setProfile = (profile: User) =>
+  ({
+    type: 'profile/loaded',
+    payload: profile,
+  } as const)
+
+export const selectProfile = (state: RootStateType) => state.profile.userData
+
+type SetProfileAT = ReturnType<typeof setProfile>
+type ActionsType = SetProfileAT
+
+//TODO: add error handling in thunk
