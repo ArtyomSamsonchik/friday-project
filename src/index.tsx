cae0bf7b58b652bea633ebe8769bb35c79655dd3
ArtyomSamsonchik <colpachek@gmail.com>
2023-01-10 08:33:33 +0300

    fix(render): remove StrictMode component from index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -34,9 +34,7 @@ const router = createHashRouter(
 const root = createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 )
