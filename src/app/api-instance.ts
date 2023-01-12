9d6f9d7cd69f2bdfc7a1d80357e3c6a0f44b30b7
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2023-01-12 23:07:18 +0300

    feat: added method for new-password request

--- src/app/api-instance.ts
+++ src/app/api-instance.ts
@@ -3,7 +3,7 @@ import axios, { AxiosResponse } from 'axios'
 export const instance = axios.create({
   baseURL:
     /*process.env.NODE_ENV === 'development'
-      ? 'http://localhost:7542/2.0'
-      : */ 'https://neko-back.herokuapp.com/2.0',
+    ? 'http://localhost:7542/2.0'
+    : */ 'https://neko-back.herokuapp.com/2.0',
   withCredentials: true,
 })
