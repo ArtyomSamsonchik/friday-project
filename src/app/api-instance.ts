ee5209380e9cde74acbfca4c14ec0f7493b74ef8
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2023-01-12 21:57:34 +0300

    refactoring: removed authApi from api-instance.ts and added to auth-api.ts

--- src/app/api-instance.ts
+++ src/app/api-instance.ts
@@ -2,39 +2,8 @@ import axios, { AxiosResponse } from 'axios'
 
 export const instance = axios.create({
   baseURL:
-    /* process.env.NODE_ENV === 'development'
-       ? 'http://localhost:7542/2.0'
-       : */ 'https://neko-back.herokuapp.com/2.0',
+    /*process.env.NODE_ENV === 'development'
+      ? 'http://localhost:7542/2.0'
+      : */ 'https://neko-back.herokuapp.com/2.0',
   withCredentials: true,
 })
-export const authApi = {
-  register(data: RegisterDataType) {
-    return instance.post<{ addedUser: any; error?: string }>('/auth/register', data)
-  },
-  login(values: LoginParamsType) {
-    return instance.post<LoginParamsType, AxiosResponse<ResponseType>>('/auth/login', values)
-  },
-}
-export type RegisterDataType = {
-  email: string
-  password: string
-}
-export type LoginParamsType = {
-  email: string
-  password: string
-  rememberMe: boolean
-}
-
-export type ResponseType = {
-  _id: string
-  email: string
-  name: string
-  avatar?: string
-  publicCardPacksCount: number
-  created: Date
-  updated: Date
-  isAdmin: boolean
-  verified: boolean
-  rememberMe: boolean
-  error?: string
-}
