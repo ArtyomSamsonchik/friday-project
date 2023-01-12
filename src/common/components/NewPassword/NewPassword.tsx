4c183afda90d5bc10d2ae202ea02741317524375
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2023-01-12 23:09:18 +0300

    feat: created New password page with form

--- src/common/components/NewPassword/NewPassword.tsx
+++ src/common/components/NewPassword/NewPassword.tsx
@@ -1,11 +1,105 @@
 import React from 'react'
 
-import commonS from '../../styles/common.module.css'
+import Visibility from '@mui/icons-material/Visibility'
+import VisibilityOff from '@mui/icons-material/VisibilityOff'
+import { IconButton, Input } from '@mui/material'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import FormControl from '@mui/material/FormControl'
+import FormGroup from '@mui/material/FormGroup'
+import FormLabel from '@mui/material/FormLabel'
+import InputAdornment from '@mui/material/InputAdornment'
+import InputLabel from '@mui/material/InputLabel'
+import TextField from '@mui/material/TextField'
+import { useFormik } from 'formik'
+import { Navigate, useParams } from 'react-router-dom'
 
-export const NewPassword = () => {
+import { PATH } from '../../../app/path'
+import { setNewPasswordTC, setNewPasswordToken } from '../../../features/auth/auth-slice'
+import { useAppDispatch, useAppSelector } from '../../../utils/hooks'
+import common from '../../styles/common.module.css'
+import { CustomPaperContainer } from '../CustomPaperContainer/CustomPaperContainer'
+import { FormikErrorType } from '../RestorePassword/RestorePassword'
+
+export const NewPassword = React.memo(() => {
+  const [showPassword, setShowPassword] = React.useState(false)
+
+  const handleClickShowPassword = () => setShowPassword(show => !show)
+
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+  }
+  const dispatch = useAppDispatch()
+  const { token } = useParams()
+  const isStateToken = useAppSelector<string>(state => state.auth.isStateToken)
+
+  const formik = useFormik({
+    initialValues: {
+      password: '',
+    },
+    validate: values => {
+      const errors: FormikErrorType = {}
+
+      if (!values.password) {
+        errors.password = 'Required'
+      } else if (values.password.length < 7) {
+        errors.password = 'Invalid password'
+      }
+
+      return errors
+    },
+    onSubmit: values => {
+      if (token) {
+        dispatch(setNewPasswordTC(values.password, token))
+      }
+    },
+  })
+
+  if (!isStateToken && !token) {
+    return <Navigate to={PATH.LOGIN} />
+  } else {
+    token && dispatch(setNewPasswordToken(token))
+  }
+
   return (
-    <div className={commonS.demo}>
-      <h1>New password</h1>
-    </div>
+    <CustomPaperContainer>
+      <FormControl fullWidth>
+        <FormLabel>
+          <h3 className={common.h3Label}>Forgot your password?</h3>
+        </FormLabel>
+        <form onSubmit={formik.handleSubmit}>
+          <FormGroup sx={{ gap: '30px' }}>
+            <FormControl fullWidth variant="standard">
+              <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+              <Input
+                id="standard-adornment-password"
+                type={showPassword ? 'text' : 'password'}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      sx={{ color: 'black' }}
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                {...formik.getFieldProps('password')}
+              />
+            </FormControl>
+            <FormLabel>
+              <p style={{ margin: '0' }}>
+                Create new password and we will send you <br /> further instructions to email
+              </p>
+            </FormLabel>
+            <Button type={'submit'} variant={'contained'} fullWidth className={common.submitBtn}>
+              Create new password
+            </Button>
+          </FormGroup>
+        </form>
+      </FormControl>
+    </CustomPaperContainer>
   )
-}
+})
