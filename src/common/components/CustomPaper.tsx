d1306dfb6b3a6e10e2c21fbede91dbf9ad106754
ArtyomSamsonchik <colpachek@gmail.com>
2023-01-15 04:41:17 +0300

    feat(styles): add custom template components
    Includes CustomPaper, CustomCard, CustomContainer.tsx

---
+++ src/common/components/CustomPaper.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { styled } from '@mui/material'
+
+export const CustomPaper = styled('div', { name: 'CustomPaper' })(({ theme }) => ({
+  borderRadius: 2,
+  boxShadow: '0 0 2px 2px rgba(0, 0, 0, 0.1)',
+  backgroundColor: theme.palette.common.white,
+}))
