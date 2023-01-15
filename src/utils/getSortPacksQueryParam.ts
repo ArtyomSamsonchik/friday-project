bfcdbead9df5c5b4e4ea59d51f85741265da30ae
ArtyomSamsonchik <colpachek@gmail.com>
2023-01-15 04:43:33 +0300

    feat(cardPack): add CardPack component
    Put demo version on a Test page

---
+++ src/utils/getSortPacksQueryParam.ts
@@ -0,0 +1,7 @@
+import { SortPacksParams } from '../features/cards/cards-api'
+
+//Use this function to get sortPacks query parameter for cardsApi
+export const getSortPacksQueryParam = ({ order, column }: SortPacksParams) =>
+  ({
+    sortPacks: (order === 'asc' ? 1 : 0) + column,
+  } as const)
