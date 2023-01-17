43c9872db9d545f1b4aebc0a2c054420867b6eb7
ArtyomSamsonchik <colpachek@gmail.com>
2023-01-17 07:51:11 +0300

    feat(loading): add progress bar component

---
+++ src/common/components/ProgressBar.tsx
@@ -0,0 +1,24 @@
+import React, { FC } from 'react'
+
+import LinearProgress, { LinearProgressProps } from '@mui/material/LinearProgress'
+
+import { selectAppStatus } from '../../app/app-slice'
+import { useAppSelector } from '../../utils/hooks'
+
+export const ProgressBar: FC<LinearProgressProps> = props => {
+  const appStatus = useAppSelector(selectAppStatus)
+
+  return appStatus === 'loading' ? (
+    <LinearProgress
+      sx={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: 6,
+        backgroundColor: 'inherit',
+      }}
+      {...props}
+    />
+  ) : null
+}
