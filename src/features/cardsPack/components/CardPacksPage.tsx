15f36083f670c28094e099d3cd0c9cce58455e77
ArtyomSamsonchik <colpachek@gmail.com>
2023-01-17 07:58:10 +0300

    feat(hooks): add useDebounce hook
    Additionally add an example of using a hook to CardPacksPage component.

--- src/features/cardsPack/components/CardPacksPage.tsx
+++ src/features/cardsPack/components/CardPacksPage.tsx
@@ -1,28 +1,41 @@
-import React from 'react'
+import React, { ChangeEvent, useEffect } from 'react'
+
+import TextField from '@mui/material/TextField'
 
 import { BackLink } from '../../../common/components/BackLink'
 import { CardsContainer } from '../../../common/components/CardsContainer'
 import { FilledButton } from '../../../common/components/FilledButton'
-import { useAppDispatch, useAppSelector } from '../../../utils/hooks'
+import { useAppDispatch, useAppSelector, useDebounce } from '../../../utils/hooks'
 import { selectProfile } from '../../profile/profile-slice'
-import { fetchCardPacksTC, selectAllPacks } from '../cards-pack-slice'
+import { fetchCardPacksTC, selectAllPacks, setPackSearchName } from '../cards-pack-slice'
 
 import { CardPack } from './cardPack/CardPack'
 
 export const CardPacksPage = () => {
   const packs = useAppSelector(selectAllPacks)
+  const packSearchName = useAppSelector(state => state.cardPacks.packSearchName)
   const profile = useAppSelector(selectProfile)
   const dispatch = useAppDispatch()
+  const debouncedTitle = useDebounce(packSearchName)
 
-  const handleLoadPacks = () => {
+  useEffect(() => {
     dispatch(fetchCardPacksTC())
+  }, [debouncedTitle])
+
+  const handleLoadPacksClick = () => {
+    dispatch(fetchCardPacksTC())
+  }
+
+  const handleSearchNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch(setPackSearchName(e.currentTarget.value))
   }
 
   return (
     <>
       {/*Toolbar with search, range slider, personal cards' toggle switch, filter reset etc*/}
       <BackLink title="test link to profile" to="/profile" />
-      <FilledButton onClick={handleLoadPacks}>load card packs</FilledButton>
+      <TextField value={packSearchName} onChange={handleSearchNameChange} />
+      <FilledButton onClick={handleLoadPacksClick}>load card packs</FilledButton>
       <CardsContainer>
         {packs.map(p => (
           <CardPack
