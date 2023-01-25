b9d664110ca305577c4d8cb15d1e8a8ac415bf60
ArtyomSamsonchik <colpachek@gmail.com>
2023-01-25 23:18:20 +0300

    fix(styles): FilledButton styles

--- src/common/components/FilledButton.tsx
+++ src/common/components/FilledButton.tsx
@@ -3,14 +3,18 @@ import React, { FC } from 'react'
 import { ButtonProps } from '@mui/material'
 import Button from '@mui/material/Button'
 
-export const FilledButton: FC<ButtonProps> = props => (
+export const FilledButton: FC<ButtonProps> = ({ sx, ...props }) => (
   <Button
     variant="contained"
     sx={{
+      p: ({ spacing }) => `${spacing(1)} ${spacing(3.5)}`,
       color: '#fff',
       textTransform: 'initial',
       backgroundColor: '#366EFF',
       borderRadius: '30px',
+      fontSize: 16,
+      lineHeight: 1.25,
+      ...sx,
     }}
     {...props}
   />
