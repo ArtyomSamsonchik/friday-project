f5e1cdfb3e57c0527f6a768da9688a0e9661d760
ArtyomSamsonchik <colpachek@gmail.com>
2023-02-09 06:36:19 +0300

    refactor(modal): add 'red' style to FilledButton component

--- src/common/components/FilledButton.tsx
+++ src/common/components/FilledButton.tsx
@@ -1,24 +1,23 @@
-import React, { FC } from 'react'
+import React from 'react'
 
-import { ButtonProps } from '@mui/material'
+import { ButtonProps, styled } from '@mui/material'
 import Button from '@mui/material/Button'
 
-export const FilledButton: FC<ButtonProps> = ({ sx, ...props }) => (
-  <Button
-    variant="contained"
-    sx={{
-      p: ({ spacing }) => `${spacing(1)} ${spacing(3.5)}`,
-      color: '#fff',
-      textTransform: 'initial',
-      backgroundColor: '#366EFF',
-      borderRadius: '30px',
-      fontSize: 16,
-      lineHeight: 1.25,
-      '&:hover': {
-        backgroundColor: '#3564DA',
-      },
-      ...sx,
-    }}
-    {...props}
-  />
-)
+export const FilledButton = styled(
+  (props: ButtonProps) => <Button variant="contained" {...props} />,
+  {
+    name: 'FilledButton',
+    shouldForwardProp: prop => prop !== 'red',
+  }
+)<{ red?: boolean }>(({ theme, red }) => ({
+  padding: `${theme.spacing(1)} ${theme.spacing(3.5)}`,
+  color: theme.palette.common.white,
+  textTransform: 'initial',
+  backgroundColor: red ? '#ec3333' : '#366EFF',
+  borderRadius: '30px',
+  fontSize: 16,
+  lineHeight: 1.25,
+  '&:hover': {
+    backgroundColor: red ? '#d21515' : '#3564DA',
+  },
+}))
