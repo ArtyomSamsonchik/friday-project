0eda964e6218eb88151bd2767f88cfc539b94e3b
iplatos <platonoff9168@gmail.com>
2023-02-09 16:25:39 +0300

    feat: learning process added

--- src/common/components/LarnCardsPage/LearnCardsPage.tsx
+++ src/common/components/LarnCardsPage/LearnCardsPage.tsx
@@ -3,12 +3,15 @@ import React, { useState } from 'react'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 
+import { PATH } from '../../../app/path'
 import { CardsNewGradeRequestData } from '../../../features/cards/cards-api'
 import { selectAllCards, selectPackName } from '../../../features/cards/cards-selectors'
 import { updateCardGradeTC } from '../../../features/cards/cards-slice'
 import { useAppDispatch } from '../../../utils/hooks/useAppDispatch'
 import { useAppSelector } from '../../../utils/hooks/useAppSelector'
-import { CustomContainer } from '../CustomContainer'
+import { BackLink } from '../BackLink'
+import { CustomPaper } from '../CustomPaper'
+import { FilledButton } from '../FilledButton'
 import { RateYourSelf } from '../RateYourSelf/RateYourSelf'
 
 export const LearnCardsPage = () => {
@@ -29,51 +32,71 @@ export const LearnCardsPage = () => {
   }
 
   return (
-    <CustomContainer>
-      <Box
+    <Box>
+      <BackLink
+        title="Back to Cards List"
+        to={`/${PATH.CARDS}/${cards[simpleCounter].cardsPack_id}`}
+      />
+      <Typography
+        align={'center'}
         sx={{
-          marginTop: '64px',
-          width: '100%',
-          height: '80%',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
+          fontSize: 22,
+          lineHeight: 1.2,
+          fontWeight: 600,
+          mb: '30px',
         }}
       >
-        <Typography>
-          <b>Learn: {packName}</b>
+        <b>Learn: {packName}</b>
+      </Typography>
+      <CustomPaper
+        sx={{
+          maxWidth: '439px',
+          minHeight: '204px',
+          display: 'flex',
+          padding: '30px',
+          flexDirection: 'column',
+          alignItems: 'center',
+          height: 'maxContent',
+          mx: 'auto',
+          animation: ({ transitions }) => `0.4s slide-up ${transitions.easing.easeInOut}`,
+        }}
+      >
+        <Typography align={'justify'}>
+          <b>Question:</b> {cards[simpleCounter].question}
         </Typography>
+        <Typography sx={{ marginTop: '30px' }} color={'grey'}>
+          Количестов попыток ответа на вопрос: {cards[simpleCounter].shots}
+        </Typography>
+
+        {showAnswer && (
+          <Typography align={'justify'} sx={{ marginTop: '30px' }}>
+            <b>Answer: </b> {cards[simpleCounter].answer}
+          </Typography>
+        )}
         <Box
           sx={{
+            marginTop: '30px',
+            width: 'maxContent',
             display: 'flex',
-            margin: '10px',
-            width: '400px',
-            height: 'max-content',
-            border: '2px solid red',
             flexDirection: 'column',
             alignItems: 'center',
-            justifyContent: 'center',
           }}
         >
-          <Typography>
-            <b>Question:</b> {cards[simpleCounter].question}
-          </Typography>
-          <Typography color={'grey'}>
-            количестов попыток ответа на вопрос: {cards[simpleCounter].shots}
-          </Typography>
-          {showAnswer && cards[simpleCounter].answer}
-          {showAnswer || <button onClick={() => setShowAnswer(!showAnswer)}>show answer</button>}
-          <RateYourSelf
-            cardId={cards[simpleCounter]._id}
-            simpleCounter={simpleCounter}
-            increaseSimpleCounter={increaseSimpleCounter}
-            getCardNewRating={getCardNewRating}
-            setGrade={setGrade}
-            grade={grade}
-          />
+          {showAnswer || (
+            <FilledButton onClick={() => setShowAnswer(!showAnswer)}>show answer</FilledButton>
+          )}
+          {showAnswer && (
+            <RateYourSelf
+              cardId={cards[simpleCounter]._id}
+              simpleCounter={simpleCounter}
+              increaseSimpleCounter={increaseSimpleCounter}
+              getCardNewRating={getCardNewRating}
+              setGrade={setGrade}
+              grade={grade}
+            />
+          )}
         </Box>
-      </Box>
-    </CustomContainer>
+      </CustomPaper>
+    </Box>
   )
 }
