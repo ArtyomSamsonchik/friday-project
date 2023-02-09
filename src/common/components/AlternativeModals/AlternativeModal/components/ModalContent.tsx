04e6968b331149f72e2916cabbb5dc1464cf2efb
ArtyomSamsonchik <colpachek@gmail.com>
2023-02-09 06:32:47 +0300

    refactor(modal): replace a lot of unnecessary modal components with AlternativeBasicModal

--- src/common/components/AlternativeModals/AlternativeModal/components/ModalContent.tsx
+++
@@ -1,5 +0,0 @@
-import { styled } from '@mui/material'
-
-export const ModalContent = styled('div', { name: 'ModalContent' })(({ theme: { spacing } }) => ({
-  padding: `${spacing(4)} ${spacing(3)} ${spacing(6)}`,
-}))
