384e30f0affc967179ffe3b3c31e2cfbc439ab2e
ArtyomSamsonchik <colpachek@gmail.com>
2023-02-14 07:27:01 +0300

    fixup! fix(cards): store update when setting toolbar options

--- src/utils/helpers/mapStateToCardsRequestParams.ts
+++ src/utils/helpers/mapStateToCardsRequestParams.ts
@@ -1,14 +1,17 @@
 import { RootStateType } from '../../app/store'
 import { GetCardsQueryParams } from '../../features/cards/cards-api'
 
-export const mapStateToCardsRequestParams = (state: RootStateType, packId: string) => {
+export const mapStateToCardsRequestParams = (state: RootStateType, params: GetCardsQueryParams) => {
   const { cardSearchName, cardsSortOrder, page, pageCount } = state.cards
 
   return {
-    cardsPack_id: packId,
     cardQuestion: cardSearchName || undefined,
     sortCards: cardsSortOrder,
     page,
     pageCount,
+    ...params,
   } as GetCardsQueryParams
 }
+
+// TODO: limit params type in future to protect fetchCards thunk of unexpected arguments.
+//  Do the same with fetchPacks thunk.
