dc5c28bb2629fd83c1c91c45be2c9a4d712552cb
iplatos <platonoff9168@gmail.com>
2023-02-21 15:19:32 +0300

    feat: style for errorPage and EmptyPage added

--- src/common/components/EmptyPackPage/EmptyPackPage.tsx
+++ src/common/components/EmptyPackPage/EmptyPackPage.tsx
@@ -3,14 +3,24 @@ import React, { useEffect } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 
 import { PATH } from '../../../app/path'
-import { selectCardsTotalCount } from '../../../features/cards/cards-selectors'
+import { selectCardsTotalCount, selectCardsUserId } from '../../../features/cards/cards-selectors'
+import { addCardTC } from '../../../features/cards/cards-slice'
+import { selectProfile } from '../../../features/profile/profile-slice'
+import { useAppDispatch } from '../../../utils/hooks/useAppDispatch'
 import { useAppSelector } from '../../../utils/hooks/useAppSelector'
 import commonS from '../../styles/common.module.css'
+import { BackLink } from '../BackLink'
+import { CustomContainer } from '../CustomContainer'
 
 export const EmptyPackPage = () => {
   const cardsTotalCount = useAppSelector(selectCardsTotalCount)
   const navigate = useNavigate()
   const { packId } = useParams()
+  const dispatch = useAppDispatch()
+  const profile = useAppSelector(selectProfile)
+  const cardsUserId = useAppSelector(selectCardsUserId)
+
+  const isMyPack = profile._id === cardsUserId
 
   console.log(cardsTotalCount)
   useEffect(() => {
@@ -19,12 +29,22 @@ export const EmptyPackPage = () => {
       navigate(`/${PATH.CARDS}/${packId}`)
     }
   }, [cardsTotalCount])
+  const addCard = () => {
+    packId && dispatch(addCardTC({ cardsPack_id: packId, question: 'I', answer: 't' }))
+  }
 
   return (
-    <div className={commonS.demo}>
-      <h1>this pack is empty</h1>
-      <h1>but you can add some card</h1>
-      <button>add card</button>
-    </div>
+    <CustomContainer>
+      <BackLink title="Back to Packs List" to={`/${PATH.PACKS}`} />
+      <div className={commonS.demo}>
+        <h1>this pack is empty</h1>
+        {isMyPack && (
+          <>
+            <h1>but you can add some card</h1>
+            <button onClick={addCard}>add card</button>
+          </>
+        )}
+      </div>
+    </CustomContainer>
   )
 }
--- src/common/components/ErrorPage/ErrorPage.tsx
+++ src/common/components/ErrorPage/ErrorPage.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
 import commonS from '../../styles/common.module.css'
+import { CustomContainer } from '../CustomContainer'
 
 export const ErrorPage = () => {
   return (
-    <div className={commonS.demo}>
-      <h1>404 Page Not Found</h1>
-    </div>
+    <CustomContainer>
+      <div className={commonS.demo}>
+        <h1>404 Page Not Found</h1>
+      </div>
+    </CustomContainer>
   )
 }
