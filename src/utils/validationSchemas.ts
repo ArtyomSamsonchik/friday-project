efca86633dbb2fdccb06a6ff5b258526024f54f6
ArtyomSamsonchik <colpachek@gmail.com>
2023-03-14 22:36:11 +0300

    feat(modal): add EditorAdd cards modal validation logic
    Additionally, update yup to 1.0.2

--- src/utils/validationSchemas.ts
+++ src/utils/validationSchemas.ts
@@ -1,5 +1,7 @@
 import * as Yup from 'yup'
 
+import { Tabs } from '../features/cards/components/EditorAddCardModal/EditorAddCardModal'
+
 export const emailSchema = Yup.string()
   .matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, 'Invalid email')
   .required('Required')
@@ -11,4 +13,22 @@ export const passwordSchema = Yup.string()
 export const loginSchema = Yup.object({
   email: emailSchema,
   password: passwordSchema,
+})
+
+const getModalInputSchema = () => {
+  return Yup.string()
+    .trim()
+    .defined()
+    .when('tab', ([tab], schema) => {
+      const message =
+        tab === Tabs.Text ? '${path} should not be empty' : 'provide an image to ${path} field'
+
+      return schema.required(message)
+    })
+}
+
+export const editorAddCardSchema = Yup.object({
+  tab: Yup.string<Tabs>().defined(),
+  question: getModalInputSchema(),
+  answer: getModalInputSchema(),
 })
