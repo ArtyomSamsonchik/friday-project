d882b46e9500c4c2eb07924fea5dc221187fc9c3
ArtyomSamsonchik <colpachek@gmail.com>
2023-03-14 23:16:45 +0300

    feat(modal): add image handling logic to EditorAddCardModal
    Refactor modal to use Formik component instead of useFormik hook. And add SwitchableModalInputs, using formik's context inside them

--- src/features/cards/components/EditorAddCardModal/EditorAddCardModal.tsx
+++ src/features/cards/components/EditorAddCardModal/EditorAddCardModal.tsx
@@ -1,144 +1,152 @@
-import React, { ChangeEvent, FC, SyntheticEvent, useState } from 'react'
+import React, { FC, memo, SyntheticEvent } from 'react'
 
-import { SxProps, Theme } from '@mui/material'
 import Tab from '@mui/material/Tab'
-import Tabs from '@mui/material/Tabs'
-import TextField, { TextFieldProps } from '@mui/material/TextField'
-import { Formik, useFormik } from 'formik'
-import { InferType, object, string, ValidationError } from 'yup'
+import MUITabs from '@mui/material/Tabs'
+import { Formik, FormikConfig } from 'formik'
+import { InferType } from 'yup'
 
 import {
-  AlternativeBasicModal,
-  AlternativeBasicModalProps,
-} from '../../../../common/components/AlternativeBasicModal/AlternativeBasicModal'
-import { ModalLabel } from '../../../../common/components/AlternativeBasicModal/styled'
+  ConfirmModal,
+  ConfirmModalProps,
+} from '../../../../common/components/ConfirmModal/ConfirmModal'
+import { ModalLabel } from '../../../../common/components/ConfirmModal/styled'
+import { coerceImage64 } from '../../../../utils/helpers/coerceImage64'
 import { useAppDispatch } from '../../../../utils/hooks/useAppDispatch'
 import { useAppSelector } from '../../../../utils/hooks/useAppSelector'
+import { editorAddCardSchema } from '../../../../utils/validationSchemas'
+import { Card, CardMutationData } from '../../cards-api'
 import { selectCard } from '../../cards-selectors'
 import { addCardTC, updateCardTC } from '../../cards-slice'
 
-enum Options {
-  Text,
-  Image,
+import { SwitchableModalInput } from './SwitchableModalInput'
+
+export enum Tabs {
+  Text = 'text',
+  Image = 'image',
 }
 
-const validationSchema = object({
-  question: string().trim().required('question should not be empty!'),
-  answer: string().trim().required('answer should not be empty!'),
-})
+enum FormStatus {
+  Initialized,
+  Switched,
+}
 
-const getTextFieldProps = (error?: string): TextFieldProps => ({
-  fullWidth: true,
-  variant: 'standard',
-  error: !!error,
-  helperText: error || ' ',
-})
+export type CardModalFormValues = InferType<typeof editorAddCardSchema>
 
-type FormValues = InferType<typeof validationSchema>
-
-type EditorAddCardModalProps = Pick<AlternativeBasicModalProps, 'isOpen' | 'title' | 'onClose'> & {
+type EditorAddCardModalProps = Pick<ConfirmModalProps, 'isOpen' | 'title' | 'onClose'> & {
   cardId: string
   packId: string
 }
 
-export const EditorAddCardModal: FC<EditorAddCardModalProps> = props => {
+export const EditorAddCardModal: FC<EditorAddCardModalProps> = memo(props => {
   const { cardId, onClose, packId, ...restProps } = props
 
   const card = useAppSelector(state => selectCard(state, cardId))
-  const initCardQuestion = card?.question || ''
-  const initCardAnswer = card?.answer || ''
-
-  const [selected, setSelected] = useState(Options.Text)
-  // const [question, setQuestion] = useState(initCardQuestion)
-  // const [answer, setAnswer] = useState(initCardAnswer)
-  // const [error, setError] = useState<string | null>(null)
   const dispatch = useAppDispatch()
 
-  const handleEditorAddCardSubmit = (values: FormValues) => {
-    const { question, answer } = validationSchema.cast(values)
-
-    console.log(question, answer)
-
-    // if (card) {
-    //   dispatch(updateCardTC({ _id: cardId, packId, question, answer }))
-    // } else {
-    //   dispatch(addCardTC({ cardsPack_id: packId, question, answer }))
-    // }
-  }
-
-  const formik = useFormik<FormValues>({
-    initialValues: {
-      question: initCardQuestion,
-      answer: initCardAnswer,
-    },
-    validationSchema,
-    validateOnChange: false,
-    validateOnBlur: false,
-    onSubmit: handleEditorAddCardSubmit,
-  })
-
-  const handleTabChange = (e: SyntheticEvent, value: Options) => {
-    setSelected(value)
-  }
-
-  const getMyFieldProps = (field: keyof FormValues) => {
-    const fieldProps = formik.getFieldProps(field)
-
-    fieldProps.onChange = (e: ChangeEvent<HTMLInputElement>) => {
-      formik.setFieldError(field, undefined)
-
-      formik.handleChange(e)
+  const getFormInitValues = (card?: Card, tab: Tabs = Tabs.Text) => {
+    const initValues: CardModalFormValues = {
+      tab,
+      question: '',
+      answer: '',
     }
 
-    return fieldProps
+    if (card) {
+      const questionImg = coerceImage64(card.questionImg)
+      const answerImg = coerceImage64(card.answerImg)
+      const isAnyValidImage = Boolean(questionImg || answerImg)
+
+      //Card modal should not display text fields if it has any image for question/answer
+      if (tab === Tabs.Text) {
+        initValues.question = isAnyValidImage ? '' : card.question
+        initValues.answer = isAnyValidImage ? '' : card.answer
+      } else {
+        initValues.question = questionImg
+        initValues.answer = answerImg
+      }
+    }
+
+    return initValues
   }
 
-  // const handleQuestionChange = (e: ChangeEvent<HTMLInputElement>) => {
-  //   setQuestion(e.currentTarget.value)
-  //   setError(null)
-  // }
-  //
-  // const handleAnswerChange = (e: ChangeEvent<HTMLInputElement>) => {
-  //   setAnswer(e.currentTarget.value)
-  //   setError(null)
-  // }
+  const handleModalSubmit: FormikConfig<CardModalFormValues>['onSubmit'] = async (
+    values,
+    { resetForm }
+  ) => {
+    const { question, answer, tab } = editorAddCardSchema.cast(values)
+    const patch = {} as CardMutationData
 
-  const handleModalClose = () => {
-    // setQuestion(initCardQuestion)
-    // setAnswer(initCardAnswer)
-    // setError(null)
-    formik.resetForm()
+    if (tab === Tabs.Text) {
+      patch.question = question
+      patch.answer = answer
+      patch.questionImg = 'no question image'
+      patch.answerImg = 'no answer image'
+    } else {
+      patch.question = 'no question'
+      patch.answer = 'no answer'
+      patch.questionImg = question
+      patch.answerImg = answer
+    }
+
+    if (card) {
+      const updatedCard = await dispatch(updateCardTC({ _id: cardId, packId, ...patch }))
+
+      if (updatedCard) resetForm({ values: getFormInitValues(updatedCard) })
+    } else {
+      await dispatch(addCardTC({ cardsPack_id: packId, ...patch }))
+    }
+
     onClose()
   }
 
   return (
-    <AlternativeBasicModal
-      primaryButtonName="Save"
-      primaryButtonIsDisabled={!formik.isValid}
-      onPrimaryButtonClick={async () => {
-        await formik.validateForm()
-        await formik.submitForm()
-      }}
-      onClose={handleModalClose}
-      {...restProps}
+    <Formik
+      initialValues={getFormInitValues(card)}
+      validationSchema={editorAddCardSchema}
+      validateOnChange={false}
+      validateOnBlur={false}
+      initialStatus={FormStatus.Initialized}
+      onSubmit={handleModalSubmit}
     >
-      <ModalLabel>Choose a question format</ModalLabel>
-      <Tabs variant="fullWidth" value={selected} sx={{ mb: 3 }} onChange={handleTabChange}>
-        <Tab value={Options.Text} label="Text" />
-        <Tab value={Options.Image} label="Image" />
-      </Tabs>
+      {({ values, isValid, isSubmitting, resetForm, handleSubmit, status, setStatus }) => {
+        const handleModalClose = () => {
+          onClose()
+          resetForm({ values: getFormInitValues(card) })
+        }
 
-      <TextField
-        label="Question"
-        {...getTextFieldProps(formik.errors.question)}
-        {...getMyFieldProps('question')}
-      />
-      <TextField
-        sx={{ mb: '-16px' }}
-        label="Answer"
-        {...getTextFieldProps(formik.errors.answer)}
-        {...getMyFieldProps('answer')}
-      />
-    </AlternativeBasicModal>
+        const handleTabChange = (e: SyntheticEvent, tab: Tabs) => {
+          resetForm({ values: getFormInitValues(card, tab) })
+          setStatus(FormStatus.Switched)
+        }
+
+        return (
+          <ConfirmModal
+            isLoading={isSubmitting}
+            primaryButtonName="Save"
+            primaryButtonIsDisabled={!isValid}
+            onPrimaryButtonClick={() => handleSubmit()}
+            onClose={handleModalClose}
+            {...restProps}
+          >
+            <ModalLabel>Choose a question format</ModalLabel>
+            <MUITabs
+              variant="fullWidth"
+              value={values.tab}
+              sx={{ mb: 3 }}
+              onChange={handleTabChange}
+            >
+              <Tab value={Tabs.Text} label="Text" />
+              <Tab value={Tabs.Image} label="Image" />
+            </MUITabs>
+            <SwitchableModalInput
+              autoFocus={status === FormStatus.Initialized && props.isOpen}
+              tab={values.tab}
+              label="question"
+              field="question"
+            />
+            <SwitchableModalInput tab={values.tab} label="answer" field="answer" />
+          </ConfirmModal>
+        )
+      }}
+    </Formik>
   )
-}
+})
