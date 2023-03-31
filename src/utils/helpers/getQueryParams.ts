bfef3d7c2e7ea8d30e9f15f2f849a60263dbbd46
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2023-03-31 15:18:28 +0300

    refactor: removed comments

--- src/utils/helpers/getQueryParams.ts
+++ src/utils/helpers/getQueryParams.ts
@@ -2,29 +2,6 @@ export const getQueryParams = (searchParams: URLSearchParams) => {
   return Object.fromEntries(searchParams)
 }
 
-// changed UrlSearchpType to GetCardPacksQueryParams
-
-// export const getQueryParamObject = <T extends {}, K extends keyof T = keyof T>(
-//   paramName: K,
-//   paramValue: T[K],
-//   searchParams: T
-// ) => {
-//   //const packNameQuery: { [key: string]: string } = paramValue ? { [paramName]: paramValue } : {}
-//   //const packNameQuery = paramValue ? { [paramName]: paramValue } : {}
-//   //const { [paramName]: param, ...otherQueries } = Object.fromEntries(searchParams)
-//   if (paramValue) {
-//     // console.log({ ...searchParams, [paramName]: paramValue })
-//
-//     return { ...searchParams, [paramName]: paramValue } as Record<string, string>
-//   } else {
-//     let copy = { ...searchParams }
-//
-//     delete copy[paramName]
-//
-//     return copy
-//   }
-// }
-
 export const getQueryParamObject = <T extends Record<string, any>>(searchParams: T) => {
   const keys = Object.keys(searchParams)
   const result = {} as Record<string, any>
@@ -33,7 +10,6 @@ export const getQueryParamObject = <T extends Record<string, any>>(searchParams:
     const value = searchParams[key]
 
     if (value) {
-      // console.log({ ...searchParams, [paramName]: paramValue })
       result[key] = String(value)
     } else {
       continue
